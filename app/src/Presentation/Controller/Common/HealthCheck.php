a6e154513b4faf2463a3a7eccb43fc11029dcfe8
Elizaveta Shevtsova <111qw95@gmail.com>
2025-07-11 00:20:19 +0300

    feat: upd gitignore

---
+++ app/src/Presentation/Controller/Common/HealthCheck.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Controller\Common;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route('/', name: 'health_check', methods: ['GET'])]
+class HealthCheck
+{
+    public function __invoke(): Response
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
+}
